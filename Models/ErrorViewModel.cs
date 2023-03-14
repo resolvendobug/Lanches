2526c53d8acddaf0abc21d9f67ab590705868500
Renato <renato@resolvendobug.com.br>
2023-03-14 16:13:36 -0300

    Add Carousel , Models Categoria , Lanches

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Lanches.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
