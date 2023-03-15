bd469fff426a009d34a298d83b8933ddfd67f445
Renato <renato@resolvendobug.com.br>
2023-03-15 11:37:25 -0300

    Add Migrations insert category table

---
+++ Migrations/20230315143051_PopularCategorias.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lanches.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, Descricao) VALUES ('Frutas da estação', 'Frutas da estação')");
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, Descricao) VALUES ('Natural', 'Lanche feito com ingredientes naturais e integrais')");
+            migrationBuilder.Sql("INSERT INTO Categorias (Nome, Descricao) VALUES ('Normal', 'Lanches feito com ingredientes normais')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
