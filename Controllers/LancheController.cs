d1d160b1b718242f50deab885ea28b754077737f
Renato Carvalho <renato@resolvendobug.com.br>
2023-03-16 22:40:52 -0300

    Add ViewModel and LancheListViewModel

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -21,8 +21,12 @@ namespace Lanches.Controllers
 
         public IActionResult List()
         {
-            var lanches = _lancheRepository.Lanches;
-            return View(lanches);
+            // var lanches = _lancheRepository.Lanches;
+            // return View(lanches);
+            var lanchesListViewModel = new ViewModels.LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+            return View(lanchesListViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
