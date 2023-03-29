1589d164dd1002077656c8d213cfd16e0a8a0cb2
Renato <renato@resolvendobug.com.br>
2023-03-29 11:32:49 -0300

    Add AccountController and LoginViewModal

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lanches.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o nome")]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }   
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
