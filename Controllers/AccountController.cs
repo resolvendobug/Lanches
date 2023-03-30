db104a1c1c141d201d8f730f8a0e1465d50ab82e
Renato <renato@resolvendobug.com.br>
2023-03-30 11:11:12 -0300

    Create AccessDenied page

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -85,5 +85,10 @@ namespace Lanches.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
     }
 }
